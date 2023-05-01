a7b0b1f76f648cae09f4b9b76a913c1257fd1405
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-01 07:53:51 +0600

    ignore node moddules

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -13,8 +14,30 @@ app.get('/', (req, res) => {
 })
 
 app.get('/categories', (req, res) => {
-    res.send(categories)
-  })
+  res.send(categories)
+})
+
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews);
+
+})
+
+app.get('/category/:id', (req, res) => {
+  const cat_id = req.params.id;
+  if (cat_id === "0") {
+    res.send(news);
+  }
+  else {
+    const categoryNews = news.filter(n => n.category_id === cat_id);
+    res.send(categoryNews);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
